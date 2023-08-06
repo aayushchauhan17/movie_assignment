75a6854d6acfbde95b6dff99e68859720875741f
aayushchauhan17 <aayushchauhan176@gmail.com>
2023-08-06 13:16:44 +0530

    host frontend

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const getData = require('./node_files/getData.js');
 const cors = require('cors'); 
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'frontend')));
+
 app.get('/',(req,res)=>{
-    res.send("Welcome!!")
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 })
 
-const port = process.env.PORT || 3000;
 
 
 app.get('/search', (req, res)=>{
